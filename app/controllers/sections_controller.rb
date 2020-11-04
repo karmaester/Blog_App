d9b6431bc3ec07d68f850ed139bc1084051373cd
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-11-04 19:32:18 -0300

    Add controller methods

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,26 @@
 class SectionsController < ApplicationController
+    def index
+    end
+
+    def new
+        @section = Section.new
+    end
+
+    def create
+        @section = Section.new(section_params)
+        if @section.save
+            redirect @section
+        else
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    private
+
+    def section_params
+        params.require(:post).permit(:name, :date)
+    end
 end
