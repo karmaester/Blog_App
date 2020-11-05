87c4283b5ab6db84940ba13a5fcc4ec9f5d3184c
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-11-05 13:24:54 -0300

    Add style to header

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_204908) do
 
   create_table "sections", force: :cascade do |t|
     t.string "name"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
