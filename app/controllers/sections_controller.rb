d996f55d75978555efc4a81cb39fbd538de364ce
Kevin Cotrina <kcotrinam92@gmail.com>
2020-11-06 12:45:52 -0500

    Add button links and partial

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -22,6 +22,18 @@ class SectionsController < ApplicationController
         @section = Section.find(params[:id])
     end
 
+    def destroy
+        @section = Section.find(params[:id])
+        if @section.destroy
+            flash[:success] = 'Object was successfully deleted.'
+            redirect_to sections_path
+        else
+            flash[:error] = 'Something went wrong'
+            redirect_to sections_path
+        end
+    end
+    
+
     private
 
     def section_params
